495463c434a92362f8b8cb8f35d51ce141600e1d
Samthebest999 <samit.mohnot@gmail.com>
2020-11-28 20:15:10 -0600

    Create Main Timer.py

---
+++ Main Timer.py
@@ -0,0 +1,5 @@
+#CountDown Timer Made by #Samthebest999
+print("Hey there, Thanks for using the countdown timer!!")
+wyltkwabw = input("So, now would you like to know why you should use this count down timer instead of one you found on the web Yes/No (Case Sensitive)")
+if wyltkwabw == "Yes":
+    
