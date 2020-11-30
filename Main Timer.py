2d236c6ebacc49ab82c68591c86daca45e3d7778
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2020-11-30 09:47:40 -0600

    Update Main Timer.py

--- Main Timer.py
+++ Main Timer.py
@@ -2,8 +2,10 @@
 print("Hey there, Thanks for using the countdown timer!!")
 wyltkwabw = input("So, now would you like to know why you should use this count down timer instead of one you found on the web Yes/No (Case Sensitive)")
 if wyltkwabw == "Yes":
-    print("""The reason you should use this countdown clock and not some clock you find on the internet is:
+    print("""
+    The reason you should use this countdown clock and not some clock you find on the internet is:
     1. It is very lightweight on that topic, it is great beacuse your browser takes a heavy load on your computer... you can leave this open in the background and it will use very little computer resources as it is coded in python.
-    2. It is very fast unlike the ones on the web browser the javascript for those websites is very complicated and has a slower execution time. My program runs on simple subtraction and displaying numbers.
-
-    
+    2. It is very fast unlike the ones on the web browser the javascript for those websites is very complicated and has a slower execution time. My program runs on simple subtraction and displaying numbers so it is much faster.
+    """)
+elif wyltkwabw == "No:
+    print("Ok We will continue")
